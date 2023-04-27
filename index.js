4217153509dc222b65e835525dbcf13db2c30da1
zebramilk <zebramilkx@gmail.com>
2023-04-27 14:50:55 -0500

    Fix container size Begin to work out selection functions and -focus classes.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const characterImages = document.querySelectorAll(".character-image");
 const pictureContainers = document.querySelectorAll(".image-container");
-const pictureFrame = document.querySelectorAll(".slide-frame");
-const carouselContainer = document.querySelectorAll(".carousel-wrapper");
+const pictureFrame = document.querySelector(".slide-frame");
+const carouselContainer = document.querySelector(".carousel-wrapper");
+
+const leftArrow = document.querySelector(".left-arrow");
+const rightArrow = document.querySelector(".right-arrow");
 
 // hide all images except 01
 pictureContainers.forEach((picture) => {
@@ -11,3 +14,30 @@ pictureContainers.forEach((picture) => {
 pictureContainers[0].classList.remove("hidden");
 
 console.table(pictureContainers);
+
+// make the imageContainers a nodelist so I can do more stuff
+const pictureContainersArray = [...pictureContainers];
+
+// toggle main-focus on the next element in images when right arrow is clicked
+function toggleMainFocus(picture) {
+  picture.classList.toggle("main-focus");
+}
+
+// toggle main focus on previous image when left arrow is clicked
+
+// toggle right-focus on next image when main-focus changes
+function toggleRightFocus(picture) {
+  picture.classList.toggle("right-focus");
+}
+// toggle left-focus on previous image when main-focus changes
+
+// default pageload things, hide all images, but apply main- right- and left-focus
+function pageLoad() {
+  toggleMainFocus(pictureContainersArray[0]);
+  to;
+}
+
+// logic for "rolling over" once the end of array is met
+if ((index = array.length)) {
+  index = 0;
+}
